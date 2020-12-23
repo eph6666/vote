f90e8319b0bc7840c0933bc38f19ccdda6b2d35e
yfwang <ephoese@gmail.com>
2020-12-23 15:48:23 +0800

    p4

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dog")
+option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
